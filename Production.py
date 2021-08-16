bea3922c0c1bc4fd7129aa5787edd6ac21596ad5
Foyjul Islam Raju <75216402+Foyjul-Islam-Raju@users.noreply.github.com>
2021-08-16 19:38:03 +0600

    Initial Commit

--- Production.py
+++ Production.py
@@ -1,20 +1,22 @@
-import scipy.optimize   #provides functions for minimizing
-               #(or maximizing) objective functions possibly subject to constraints
+import scipy.optimize   
+#provides functions for minimizing
+#(or maximizing) objective functions possibly subject to constraints
 
 # Objective Function: 50x_1 + 80x_2
 # Constraint 1: 5x_1 + 2x_2 <= 20
 # Constraint 2: -10x_1 + -12x_2 <= -90
 
-result = scipy.optimize.linprog(   # minimize a linear objective function subject
-                                   # to linear equality and inequality constraints.
+# minimize a linear objective function subject
+# to linear equality and inequality constraints.
+result = scipy.optimize.linprog(   
     [50, 80],  # Cost function: 50x_1 + 80x_2
     A_ub=[[5, 2], [-10, -12]],  # Coefficients for inequalities
     b_ub=[20, -90],  # Constraints for inequalities: 20 and -90  #ub=upperbound
 )
 
-if result.success:     #result success hole result show korbe
-    print(f"X1: {round(result.x[0], 2)} hours")   #f"X1 eta f string formatting
-    print(f"X2: {round(result.x[1], 2)} hours")   #rounding result er jonno jehetu
-                                    # 2 deya tai 2 decimal point porjonto result dekhabe
+if result.success:     
+    print(f"X1: {round(result.x[0], 2)} hours")  
+    print(f"X2: {round(result.x[1], 2)} hours")  
+                                  
 else:
-    print("No solution")   #result success na hole no solution dekhabe
+    print("No solution")
